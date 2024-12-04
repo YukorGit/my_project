82fa5ff6d4deec4091bd888c9c3355fb31aa21c8
Yuriy <yuriyseraz@gmail.com>
2024-12-04 15:35:14 +0300

    gr

--- first_project/routes/web.php
+++ first_project/routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    return 'ID: ' . $id . '. Name: ' . $name;
+});
+
